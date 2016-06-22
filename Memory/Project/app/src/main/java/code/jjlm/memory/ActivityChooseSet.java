3a0c089f8ba90087726c7f96c6ca13da2ed4685d
Jens V <jens@northcode.no>
2016-06-22 11:42:53 +0200

    Layouts from luca

---
+++ Memory/Project/app/src/main/java/code/jjlm/memory/ActivityChooseSet.java
@@ -0,0 +1,18 @@
+package code.jjlm.memory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jens on 22.06.2016.
+ */
+public class ActivityChooseSet extends AppCompatActivity {
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
---
+++ Memory/Project/app/src/main/java/code/jjlm/memory/ActivityMPEndscreen.java
@@ -0,0 +1,17 @@
+package code.jjlm.memory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jens on 22.06.2016.
+ */
+public class ActivityMPEndscreen extends AppCompatActivity {
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+}
